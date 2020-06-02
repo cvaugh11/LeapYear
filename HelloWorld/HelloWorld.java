8b7743e6fbf9515a8314ca9019ef2a182138699d
Carlton Vaughn <62064666+cvaugh11@users.noreply.github.com>
2020-06-02 05:57:40 -0400

    Delete HelloWorld.java

--- HelloWorld/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld {
-    public String getMessage() {
-        return "Hello, World!";
-    }
-    public String getMessage(String name) {
-        return "Hello, " + name + "!";
-    }
-}
